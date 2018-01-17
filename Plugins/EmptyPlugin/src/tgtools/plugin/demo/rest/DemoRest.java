4e282bb47f9fa7bf13b174e94b763d590b9096f8
田径 <tian___jing@hotmail.com>
2018-01-17 09:32:21 +0800

    增加demo

--- Plugins/EmptyPlugin/src/tgtools/plugin/demo/rest/DemoRest.java
+++ Plugins/EmptyPlugin/src/tgtools/plugin/demo/rest/DemoRest.java
@@ -3,6 +3,8 @@ package tgtools.plugin.demo.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import tgtools.json.JSONException;
+import tgtools.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,4 +29,24 @@ public class DemoRest {
             e.printStackTrace();
         }
     }
+    //访问路径：http://ip/EmptyWeb/myrest/DemoRest/hello
+    @RequestMapping(value = "/hellojson",method= {RequestMethod.GET,RequestMethod.POST})
+    public void hellojson(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            JSONObject json =new JSONObject();
+            json.put("aa","11");
+            json.put("bb",22);
+            json.put("cc","33");
+            JSONObject d=new JSONObject();
+            d.put("da","a");
+            json.put("dd",d);
+            response.setContentType("application/json");
+            response.getOutputStream().write(json.toString().getBytes("UTF-8"));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
